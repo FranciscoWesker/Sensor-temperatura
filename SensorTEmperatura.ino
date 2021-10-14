87d795b53a2bfcd550b7f23dd0561967ad8d8123
FranciscoWesker <91083820+FranciscoWesker@users.noreply.github.com>
2021-10-13 20:30:56 -0500

    Solucionando errores

--- SensorTEmperatura.ino
+++ SensorTEmperatura.ino
@@ -23,12 +23,12 @@ if (isnan(h) || isnan(t) || isnan(f) {
   float hic = dht.computeHeatIndex(t,h, false);
   Serial.print("Humedad: ");
   Serial.print(h);
-  Serial.print(" %╲t");
+  Serial.print(" %\t");
   Serial.print("Temperatura: ");
   Serial.print(t);
   Serial.print(" *C ");
   Serial.print(f);
-  Serial.print(" *F╲t");
+  Serial.print(" *F\t");
   Serial.print("Indice de calor: ");
   Serial.print(hic);
   Serial.print(" *C ");
